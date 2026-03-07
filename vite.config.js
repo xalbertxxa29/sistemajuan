44154ebeb51fd20a46b0c85d92b3fe943ca154ab
Juan Alberto Solis <jsolis@liderman.com>
2026-03-07 10:29:39 -0500

    hecho
    hecho

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+    base: '/sistemajuan/',
     build: {
         rollupOptions: {
             input: {
